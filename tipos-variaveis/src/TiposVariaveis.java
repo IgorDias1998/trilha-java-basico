dcc1542c1fd9094482cbcae5e2174637271d2eaa
IgorDias1998 <igordiass1998@gmail.com>
2024-03-12 11:00:46 -0300

    Update "declarações de variáveis"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Variáveis
+        double salarioMinimo = 2500.55;
+        short ano = 2024;
+        float pi = 3.14f;
+        String nome = "Igor";
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        //Constantes (Valores não mudam \ Adicionar final antes do tipo da variável \ Nomear em caixa alta)
+        final short IDADE = 24;
     }
 }
