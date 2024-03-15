5a28facefa31ad7a22278816952d0aa7471a7aaf
IgorDias1998 <igordiass1998@gmail.com>
2024-03-15 17:10:57 -0300

    "Criando class SmartTV"

---
+++ tipos-variaveis/src/smarttv/SmartTv.java
@@ -0,0 +1,31 @@
+package smarttv;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 20;
+
+    public void ligar() {
+        ligada = true;
+    }
+    
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void mudarCanal(int novoCanal) {
+        System.out.println("Canal atual: " + canal);
+        canal = novoCanal;
+        System.out.println("Novo canal: " + canal);
+    }
+}
